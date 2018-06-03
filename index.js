a33397bbba34106cef965def4b2ebb779652f60f
Sean Huang <hyprul@ucla.edu>
2018-06-03 00:20:21 -0500

    Done.

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ function nestedTarget(){
 function increaseRankBy(n){
 	const rankedLists = document.querySelectorAll('.ranked-list')
 
-	for(let i = 0, l = rankedLists.length; i < l; i++){
+	for(let i = 0; i < rankedLists.length; i++){
 		let children = rankedLists[i].children
 
-		for (let j = 0, k = children.length; j < k; j++){
+		for (let j = 0; j < children.length; j++){
 			children[j].innerHTML = parseInt(children[j].innerHTML) + n
 		}
 	}
